26d8d013c0f8374ba98da2a284cb7bc64f567960
gopalrana <57245908+gopalrana@users.noreply.github.com>
2025-03-12 04:54:48 +0000

    new code added

--- index.js
+++ index.js
@@ -84,6 +84,22 @@ app.delete('/api/newaddedbooks/:id', async (req, res) => {
 });
 
 
+
+// POST a new book
+app.post('/api/books', async (req, res) => {
+  try {
+    const { title, author, genre } = req.body;
+    const newBook = new Book({ title, author, genre });
+    const savedBook = await newBook.save();
+    res.status(201).json(savedBook);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server Error' });
+  }
+});
+
+
+
 // heeello
 
 const PORT = process.env.PORT || 3000;
